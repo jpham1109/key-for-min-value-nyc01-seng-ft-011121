fbee5849b4e702b851f1f79dfb02ca5f125cecea
Jamie Lawrence-Hurt <jpham1109@gmail.com>
2021-01-03 19:52:41 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -7,8 +7,8 @@ def key_for_min_value(name_hash)
   name_hash.each do |key, value|
     if min_value > value
       min_value = value 
+      key_for_min = key
     end 
-    
   end
-  
+  key_for_min
 end
